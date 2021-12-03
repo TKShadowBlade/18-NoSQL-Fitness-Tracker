9f8f521a7e6a83d405da0581e3371525c29b65fa
TKShadowBlade <katanablack85@gmail.com>
2021-12-03 17:45:25 -0500

    api routing updates

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,5 +9,20 @@ module.exports = (app) => {
             res.status(400).json(err);
         });
     });
-
+    app.get('/api/workouts/range', ({}, res) => {
+        db.workout.find({}).then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+    app.post('/api/workouts/', (req, res) => {
+        db.workout.create(req.body).then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    });
 }
