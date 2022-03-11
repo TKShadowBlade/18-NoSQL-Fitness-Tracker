b3160275ac45a8d1dbb68bece46a532dde434657
TKShadowBlade <katanablack85@gmail.com>
2022-03-11 17:40:46 -0500

    updates to server.js

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 const express = require('express');
+const logger = require('morgan');
 const mongoose = require('mongoose');
+;
 
-const htmlRoutes = require('./routes/htmlroutes');
-const apiRoutes = require('./routes/apiroutes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 const app = express();
+
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.mongodb_uri || 'mongodb://localhost/workout', {
+mongoose.connect('mongodb://localhost/workout', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
 });
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(require('./routes/apiroutes'));
+app.use(require('./routes/htmlroutes'));
 
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}`)
